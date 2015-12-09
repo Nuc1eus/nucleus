223ca99462cf2743b26738fed9ebfea3228054d5
Paul Boyle <pboyle17@gmail.com>
2015-12-09 12:45:03 -0600

    crud for categories

--- controllers/category.rb
+++ controllers/category.rb
@@ -8,23 +8,19 @@ class CategoriesController < ApplicationController
 
     post '/' do
 
-      @product =Item.create(product_name: params[:product_name],product_description: params[:product_description], product_upc: params[:product_upc], product_number: params[:product_number], product_vendor: params[:product_vendor], unit_price: params[:unit_price], product_image: params[:product_image],category_id: params[:category_id])
-
-
-      @products=Item.all
-      return @products.to_json
-    end
-
-
-    get '/add' do
-      erb :categories
-    end
-
-    post '/add' do
-      @category=Category.create(category_name: params[:category_name])
+      @category=Category.new
+      @category.category_name=params[:category_name]
+      @category.save
       @categories=Category.all
+      binding.pry
       return @categories.to_json
+
     end
+
+
+
+
+
 
 
 end
