fe2749376ef95f81f8f6a78d2029656e4fd0bfd5
Paul Boyle <pboyle17@gmail.com>
2015-12-11 10:06:46 -0600

    dropdowns for category and location ids working!

--- controllers/item.rb
+++ controllers/item.rb
@@ -32,13 +32,14 @@ class ItemsController < ApplicationController
   end
 
   get '/update/:id' do
-    binding.pry
+    @categories=Category.all
+    @locations=Location.all
+    @products=Item.all
     @product = Item.find(params[:id])
     erb :update
   end
 
   post '/update' do
-    binding.pry
     @product = Item.find(params[:id])
     @product.product_name = params[:product_name]
     @product.product_description = params[:product_description]
@@ -48,26 +49,11 @@ class ItemsController < ApplicationController
     @product.unit_price = params[:unit_price]
     @product.product_image = params[:product_image]
     @product.product_qty = params[:product_qty]
-    # @product.category_id = params[:category_id]
-    # @product.location_id = params[:location_id]
+    @product.category_id=params[:category_id]
+    @product.location_id=params[:location_id]
     @product.save
     redirect '/dash/list'
   end
-  #
-  # post '/confirmation' do
-  #   @product = Item.find(params[:id])
-  #   @product.product_name=params[:product_name]
-  #   @product.product_description=params[:product_description]
-  #   @product.product_upc=params[:product_upc]
-  #   @product.product_number=params[:product_number]
-  #   @product.product_vendor=params[:product_vendor]
-  #   @product.unit_price=params[:unit_price]
-  #   @product.product_image=params[:product_image]
-  #   @product.category_id=params[:category_id]
-  #   @product.location_id=params[:location_id]
-  #   @product.save
-  #   @product.to_json
-  # end
 
   post '/sell/:id' do
     @item = Item.find(params[:id])
