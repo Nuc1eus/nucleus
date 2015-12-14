63645338f81d2bdbbd39dbdf92afe33458033ddf
Paul Boyle <pboyle17@gmail.com>
2015-12-14 10:42:12 -0600

    seed file

--- db/seed.sql
+++ db/seed.sql
@@ -14,41 +14,63 @@
   INSERT INTO categories (category_name) VALUES ('wheels');
   INSERT INTO categories (category_name) VALUES ('tires');
   INSERT INTO categories (category_name) VALUES ('batteries');
-  INSERT INTO categories (category_name) VALUES ('radiators');
+  INSERT INTO categories (category_name) VALUES ('misc');
 
-  INSERT INTO products (product_name, product_vendor, category_id, location_id, unit_price, product_qty) VALUES ('royal purple','auto zone', 1, 4, 9.95, 30);
+  INSERT INTO products (product_name, product_vendor, category_id, location_id, unit_price, product_qty) VALUES ('royal purple','auto zone', 1, 4, 5.55, 30);
 
-  INSERT INTO products (product_name, product_vendor, category_id, location_id, unit_price, product_qty) VALUES ('die hard gold','auto zone', 11, 2, 140, 8);
+  INSERT INTO products (product_name, product_vendor, category_id, location_id, unit_price, product_qty) VALUES ('die hard gold','auto zone', 6, 2, 140, 8);
 
-  INSERT INTO products (product_name, product_vendor, category_id, location_id, unit_price, product_qty) VALUES ('penzoil','auto zone', 1, 1, 8, 10);
+  INSERT INTO products (product_name, product_vendor, category_id, location_id, unit_price, product_qty) VALUES ('penzoil','auto zone', 1, 1, 8, 1);
 
   INSERT INTO products (product_name, product_vendor, category_id, location_id, unit_price, product_qty) VALUES ('mobil 1','auto zone', 1, 7, 24.66, 18);
 
-  INSERT INTO products (product_name, product_vendor, category_id, location_id, unit_price, product_qty) VALUES ('powerstop','auto zone', 7, 3, 130.00, 4);
+  INSERT INTO products (product_name, product_vendor, category_id, location_id, unit_price, product_qty) VALUES ('powerstop','auto zone', 2, 3, 130.00, 4);
 
-  INSERT INTO products (product_name, product_vendor, category_id, location_id, unit_price, product_qty) VALUES ('wilwood','auto zone', 7, 6, 750.00, 1  );
+  INSERT INTO products (product_name, product_vendor, category_id, location_id, unit_price, product_qty) VALUES ('wilwood','auto zone', 2, 6, 750.00, 1  );
 
-  INSERT INTO products (product_name, product_vendor, category_id, location_id, unit_price, product_qty) VALUES ('sparco assetto','tire rack', 9, 8, 68.00, 4);
+  INSERT INTO products (product_name, product_vendor, category_id, location_id, unit_price, product_qty) VALUES ('sparco assetto','tire rack', 4, 7, 68.00, 4);
 
-  INSERT INTO products (product_name, product_vendor, category_id, location_id, unit_price, product_qty) VALUES ('walker','tire rack', 9, 8, 125.00, 4);
+  INSERT INTO products (product_name, product_vendor, category_id, location_id, unit_price, product_qty) VALUES ('walker','tire rack', 4, 7, 125.00, 4);
 
-  INSERT INTO products (product_name, product_vendor, category_id, location_id, unit_price, product_qty) VALUES ('mthod nv ','auto zone', 9, 1, 200, 1);
+  INSERT INTO products (product_name, product_vendor, category_id, location_id, unit_price, product_qty) VALUES ('mthod nv ','auto zone', 5, 1, 200, 1);
 
-  INSERT INTO products (product_name, product_vendor, category_id, location_id, unit_price, product_qty) VALUES ('royal purple','auto zone', 1, 4, 9.95, 30);
+  INSERT INTO products (product_name, product_vendor, category_id, location_id, unit_price, product_qty) VALUES ('royal purple','auto zone', 1, 4, 5.55, 30);
 
-  INSERT INTO products (product_name, product_vendor, category_id, location_id, unit_price, product_qty) VALUES ('douglas performance gt','tire rack', 10, 2, 52.50, 2);
+  INSERT INTO products (product_name, product_vendor, category_id, location_id, unit_price, product_qty) VALUES ('douglas performance gt','tire rack', 5, 2, 52.50, 2);
 
-  INSERT INTO products (product_name, product_vendor, category_id, location_id, unit_price, product_qty) VALUES ('Bf Goodrich','tire rack', 10, 1, 199.49, 1);
+  INSERT INTO products (product_name, product_vendor, category_id, location_id, unit_price, product_qty) VALUES ('Bf Goodrich','tire rack', 5, 1, 155.45, 1);
 
-  INSERT INTO products (product_name, product_vendor, category_id, location_id, unit_price, product_qty) VALUES ('continental','tire rack', 10, 8, 87.25, 1);
+  INSERT INTO products (product_name, product_vendor, category_id, location_id, unit_price, product_qty) VALUES ('continental','tire rack', 5, 5, 57.25, 1);
 
-  INSERT INTO products (product_name, product_vendor, category_id, location_id, unit_price, product_qty) VALUES ('continental','tire rack', 10, 8, 87.25, 1);
+  INSERT INTO products (product_name, product_vendor, category_id, location_id, unit_price, product_qty) VALUES ('continental','tire rack', 5, 5, 57.25, 1);
 
-  INSERT INTO products (product_name, product_vendor, category_id, location_id, unit_price, product_qty) VALUES ('continental','tire rack', 10, 8, 87.25, 1);
+  INSERT INTO products (product_name, product_vendor, category_id, location_id, unit_price, product_qty) VALUES ('continental','tire rack', 5, 5, 87.25, 1);
 
+  INSERT INTO products (product_name, product_vendor, category_id, location_id, unit_price, product_qty) VALUES (' euro style headlights','spyder', 7, 5, 76.45, 1);
 
+  INSERT INTO products (product_name, product_vendor, category_id, location_id, unit_price, product_qty) VALUES ('2016 nissan altima air filter','k & n', 7, 5, 57.25, 1);
 
+  INSERT INTO products (product_name, product_vendor, category_id, location_id, unit_price, product_qty) VALUES ('roadside emergency kit','k & n', 7, 5, 57.25, 1);
 
+  INSERT INTO products (product_name, product_vendor, category_id, location_id, unit_price, product_qty) VALUES ('BFGoodrich Radial Tires All season','tire rack', 5, 5, 57.25, 1);
+
+  INSERT INTO products (product_name, product_vendor, category_id, location_id, unit_price, product_qty) VALUES ('callahan auto parts tee shirt','callahan', 7, 5, 57.25, 1);
+
+  INSERT INTO products (product_name, product_vendor, category_id, location_id, unit_price, product_qty) VALUES ('goodyear fortera hl 245/65-17','tire rack', 5, 5, 57.25, 1);
+
+  INSERT INTO products (product_name, product_vendor, category_id, location_id, unit_price, product_qty) VALUES ('michelin defender 215/65R16 all season','tire rack', 5, 5, 57.25, 1);
+
+  INSERT INTO products (product_name, product_vendor, category_id, location_id, unit_price, product_qty) VALUES ('michelin primacy','tire rack', 5, 5, 136.57, 1);
+
+  INSERT INTO products (product_name, product_vendor, category_id, location_id, unit_price, product_qty) VALUES ('continental extreme contact dws','tire rack', 5, 5, 145.20, 1);
+
+  INSERT INTO products (product_name, product_vendor, category_id, location_id, unit_price, product_qty) VALUES ('oil filter wrench','craftsman', 7, 5, 11.65, 1);
+
+  INSERT INTO products (product_name, product_vendor, category_id, location_id, unit_price, product_qty) VALUES ('continental pure contact','tire rack', 5, 5, 134.1, 1);
+
+  INSERT INTO products (product_name, product_vendor, category_id, location_id, unit_price, product_qty) VALUES ('goodyear eagle 235/45-15','tire rack', 5, 5, 12.1, 1);
+
+  INSERT INTO products (product_name, product_vendor, category_id, location_id, unit_price, product_qty) VALUES ('roadside emergency kit','k & n', 7, 5, 57.25, 1);
 
 SELECT * FROM categories;
 SELECT * FROM categories;
